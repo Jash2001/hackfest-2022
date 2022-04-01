1a77582b7d809fb3fc47d117ae0cd8e993dcacbc
SanjayDevTech <sureshsanjay805@gmail.com>
2022-04-01 21:17:49 +0530

    button bg changed

--- src/components/register.js
+++ src/components/register.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './registerstyle.css';
 
 function Register() {
-    return(
-        <div className='register-btn'>
-                <a href ="https://forms.gle/tU6QUA8yrmgdwXn5A"> Register Here </a>
-        </div>
-    );
+	return (
+		<div className='register-bg'>
+			<button className='register-btn'>
+				<a target='_blank' href='https://forms.gle/tU6QUA8yrmgdwXn5A'>
+					Register Here
+				</a>
+			</button>
+		</div>
+	);
 }
 
 export default Register;
