5a338197e47401e13fda2586da59c473c0491f9a
Shivani R <2001.srkmaysrk@gmail.com>
2022-04-06 16:31:09 +0530

    poster and registration status updated

--- src/components/register.js
+++ src/components/register.js
@@ -4,11 +4,7 @@ import './registerstyle.css';
 function Register() {
 	return (
 		<div className='register-bg'>
-			<button className='register-btn'>
-				<a target='_blank' href='https://forms.gle/tU6QUA8yrmgdwXn5A'>
-					Register Here
-				</a>
-			</button>
+			<div className='register-btn'>Registration Closed</div>
 			<button className='register-btn'>
 				<a href='/template.pptx' download>
 					Download Template
